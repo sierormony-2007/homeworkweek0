a138e5d2e37b24b3a3cb7e29dd2da1f91884b339
Sie Rormony <niin2015@gmail.com>
2025-10-04 09:42:55 +0700

    homeworkweek0

--- week0.cpp
+++ week0.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
 using namespace std;
 void insertNumber(int array[], int pos, int val, int size){
     if(pos<0||pos>size){
         cout<<"Error!"<<endl;
         return;
-    }for(int i=size; i>pos; i--){
+    }
+    for(int i=size; i>pos; i--){
         array[i]=array[i-1];
     }
     array[pos]=val;
@@ -24,15 +24,21 @@ void deleteNumber(int array[], int pos, int& size){
 int main(){
     int a[10]={5,4,3,2,1};
     int size=5;
+
+    cout<<"Original Number: ";
+    for(int i=0; i<size; i++){
+        cout<<a[i]<<" ";
+    }
+    
+    cout<<endl<<"After Insertion: ";
     insertNumber(a, 2, 10, size);
     for(int i=0; i<=size; i++){
         cout<<a[i]<<" ";
-    }cout<<endl;
+    }
     
+    cout<<endl<<"After Deletion: ";
     deleteNumber(a, 2, size);
     for(int i=0; i<size; i++){
         cout<<a[i]<<" ";
-
     }
-    return 0;
 }
